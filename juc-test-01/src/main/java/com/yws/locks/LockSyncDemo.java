24ab8c7cb648b5b1d9f8555945b9c1ab9a635992
yws <yws528819@163.com>
2022-07-20 21:50:17 +0800

    多线程锁 test

--- juc-test-01/src/main/java/com/yws/locks/LockSyncDemo.java
+++ juc-test-01/src/main/java/com/yws/locks/LockSyncDemo.java
@@ -4,10 +4,18 @@ public class LockSyncDemo {
 
     Object object = new Object();
 
-    public  void m1() {
+/*    public void m1() {
         synchronized (object) {
             System.out.println("---hello synchronized code block");
         }
+    }*/
+
+    public synchronized void m2() {
+        System.out.println("---hello synchronized m2");
+    }
+
+    public static synchronized void m3() {
+        System.out.println("---hello synchronized m3");
     }
 
     public static void main(String[] args) {
